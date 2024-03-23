d063f9c3e1543e60ea1d2cd434dc1f34627f5781
Harsh Upadhyay <harsh9756416665@gmail.com>
2024-03-23 15:01:05 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
-import GameBoard from "./components/GameBoard"
-import Player from "./components/player"
-import Logs from "./components/Logs"
+import GameBoard from "./components/GameBoard.jsx"
+import Player from "./components/Player.jsx"
+import Logs from "./components/Logs.jsx"
 import { WINNING_COMBINATIONS } from "./winner"
-import GameOver from "./components/GameOver"
+import GameOver from "./components/GameOver.jsx"
 
 const initialGameBoard = [
   [null, null, null],
